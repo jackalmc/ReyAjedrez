76d52b721806861268e2d702e745bf5e1e12e737
Jackalmc <almagrojj90.it@gmail.com>
2023-11-14 13:30:24 +0100

    Added: equals() y hash

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import java.util.Objects;
+
 public class Posicion {
     private int fila;
     private char columna;
@@ -39,6 +41,16 @@ public class Posicion {
         return columna;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Posicion posicion = (Posicion) o;
+        return fila == posicion.fila && columna == posicion.columna;
+    }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(fila, columna);
+    }
 }
