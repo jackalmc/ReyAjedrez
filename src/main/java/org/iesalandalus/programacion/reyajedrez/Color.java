d6775c18c7fd038e8c133f59c9b78c23f255b9f4
Jackalmc <almagrojj90.it@gmail.com>
2023-11-14 15:03:08 +0100

    Added: mover(), comprobarEnroque(). Fixed: pequeños errores y ajustes de toString().

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Color.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Color.java
@@ -3,16 +3,14 @@ package org.iesalandalus.programacion.reyajedrez;
 public enum Color {
     BLANCO("Blanco"), NEGRO("Negro");
 
-    private String cadenaAMostrar;
+    private final String cadenaAMostrar;
 
     private Color(String cadenaAMostrar) {
         this.cadenaAMostrar = cadenaAMostrar;
     }
     @Override
     public String toString() {
-        return "Color{" +
-                "cadenaAMostrar='" + cadenaAMostrar + '\'' +
-                '}';
+        return "Color= " + cadenaAMostrar;
     }
 
 
--- src/main/java/org/iesalandalus/programacion/reyajedrez/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Rey.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Rey {
 
     private Color color;
@@ -40,5 +42,41 @@ public class Rey {
 
     private void setTotalMovimientos(int totalMovimientos) {
         this.totalMovimientos = totalMovimientos;
+    }
+
+    private boolean comprobarEnroque() throws OperationNotSupportedException{
+        if (getTotalMovimientos() != 0)
+            throw new OperationNotSupportedException("El Rey no está en su posición inicial");
+        return true;
+    }
+
+    public void mover(Direccion direccion) throws NullPointerException, OperationNotSupportedException {
+        if (direccion == null)
+            throw new NullPointerException("No se permite direcciones nulas");
+        else {
+            switch (direccion) {
+                case NORTE -> posicion = new Posicion(posicion.getFila() + 1, posicion.getColumna());
+                case NORESTE -> posicion = new Posicion(posicion.getFila() + 1, (char) (posicion.getColumna() + 1));
+                case ESTE -> posicion = new Posicion(posicion.getFila(), (char) (posicion.getColumna() + 1));
+                case SURESTE -> posicion = new Posicion(posicion.getFila() - 1, (char) (posicion.getColumna() + 1));
+                case SUR -> posicion = new Posicion(posicion.getFila() - 1, posicion.getColumna());
+                case SUROESTE -> posicion = new Posicion(posicion.getFila() - 1, (char) (posicion.getColumna() - 1));
+                case OESTE -> posicion = new Posicion(posicion.getFila(), (char) (posicion.getColumna() - 1));
+                case NOROESTE -> posicion = new Posicion(posicion.getFila() + 1, (char) (posicion.getColumna() - 1));
+                case ENROQUE_CORTO -> {
+                    if (comprobarEnroque())
+                        posicion = new Posicion(posicion.getFila(), (char) (posicion.getColumna() + 2));
+                }
+                case ENROQUE_LARGO -> {
+                    if (comprobarEnroque())
+                        posicion = new Posicion(posicion.getFila(), (char) (posicion.getColumna() - 2));
+                }
+            }
+            setTotalMovimientos(getTotalMovimientos()+1);
+        }
+
+
+
+
     }
 }
