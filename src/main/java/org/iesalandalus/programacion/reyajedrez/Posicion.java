2aeb98b30642a2f78706edd2af04b2c1c658a1d2
Jackalmc <almagrojj90.it@gmail.com>
2023-11-14 13:13:39 +0100

    Added: getters y setters, con comprobación de posición correcta.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
@@ -4,7 +4,31 @@ public class Posicion {
     private int fila;
     private char columna;
 
+    public void setFila(int fila) {
+        if (fila>8)
+            throw new IllegalArgumentException("La fila no puede ser mayor que 8");
+        else if (fila < 1)
+            throw new IllegalArgumentException("La fila no puede ser menor que 1");
+        else
+            this.fila = fila;
+    }
 
+    public void setColumna(char columna) {
+        if (columna > 'h')
+            throw new IllegalArgumentException("La columna no puede ser mayor que 8");
+        else if (columna < 'a')
+            throw new IllegalArgumentException("La columna no puede ser menor que 1");
+        else
+            this.columna = columna;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
 
 
 }
