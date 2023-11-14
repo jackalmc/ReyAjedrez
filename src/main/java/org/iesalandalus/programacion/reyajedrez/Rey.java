d00479825c53ef6784b5e8e1e9f83cba93def7ae
Jackalmc <almagrojj90.it@gmail.com>
2023-11-14 13:36:54 +0100

    Added: class Rey -> getters y setters

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Rey.java
@@ -6,5 +6,27 @@ public class Rey {
     private Posicion posicion;
     private int totalMovimientos;
 
+    public Color getColor() {
+        return color;
+    }
 
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+
+    public int getTotalMovimientos() {
+        return totalMovimientos;
+    }
+
+    public void setTotalMovimientos(int totalMovimientos) {
+        this.totalMovimientos = totalMovimientos;
+    }
 }
