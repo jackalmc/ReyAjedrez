07eeb930df501e538a18caa9097983475f7d94b1
Jackalmc <almagrojj90.it@gmail.com>
2023-11-15 11:14:50 +0100

    Added: mostrarMenu() y elegirOpcionMenu().

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
@@ -1,7 +1,28 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
+import java.sql.SQLOutput;
+
 public class Consola {
 
     public Consola(){
+    }
+
+    //Varios sout en vez de consolidar por claridad al leerlo, pero normalmente lo reduciría a una línea usando métodos de formato.
+    public static void mostrarMenu(){
+        System.out.println("1. Crear rey por defecto.");
+        System.out.println("2. Crear rey eligiendo color.");
+        System.out.println("3. Mover.");
+        System.out.println("4. Salir.");
+    }
+
+    public static int elegirOpcionMenu(){
+        int opcion;
+
+        System.out.println("====");
+        System.out.print("Introduzca elección (1-4): ");
+        opcion = Entrada.entero();
+        return opcion;
     }
 }
